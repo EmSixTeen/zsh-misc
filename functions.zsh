1f265af06b3a203336be576a1a1e6e9ac44905ec
Aaron Gregg <emsixteen@gmail.com>
2022-06-15 16:50:00 +0200

    Fixed: Relative path sourcing

--- functions.zsh
+++ functions.zsh
@@ -5,31 +5,35 @@
 # Putting emulate -LR zsh near the beginning of a function definition is good 
 # hygiene when the function is meant to be used in contexts where options are 
 # potentially different, especially in interactive shells.
-# ( https://unix.stackexchange.com/a/372866 )
+# - https://unix.stackexchange.com/a/372866
 emulate -LR zsh
 
 # Functions
 # =========
-source "$PWD/functions/go-to-site.zsh"
 
-## Reload ZSH
+# Source files
+# - This is not portable, but works for me so..
+#   https://stackoverflow.com/a/6659815/493159
+source $(dirname $0)/functions/go-to-site.zsh
+
+# Reload ZSH
 function reload() {
     exec zsh
 }
 
-## Change to a directory and then show the files in it
+# Change to a directory and then show the files in it
 function cdl() {
     cd $1 && ls
     alias cdls="cdl"
 }
 
-## Make directory & enter it
+# Make directory & enter it
 function mkcd() {
     mkdir -p $1
     cd $1
 }
 
-## Edit your hosts file
+# Edit your hosts file
 function edithosts() {
     sudo vim /etc/hosts
 }
